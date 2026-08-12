cb93382ffb2a4ae6db953dbd1c886ddcf536f014
bmalhotrabe23-code <bmalhotra_be23@thapar.edu>
2026-08-12 15:13:08 +0530

    Update baud_gen.v

--- baud_gen.v
+++ baud_gen.v
@@ -1,28 +1,47 @@
-module baud_gen #(
-    parameter CLK_FREQ = 50000000,   // 50 MHz
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.07.2026 18:38:32
+// Design Name: 
+// Module Name: baud_rate_gneraor_for_tx_Rx_2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module baud_rate_gneraor_for_tx_Rx_2 #(
+    parameter CLK_FREQ = 100_000_000,
     parameter BAUD_RATE = 9600
 )(
     input clk,
     input reset,
-    output reg tick
+    output baud_tick  
 );
+   // parameter CLK_FREQ = 100_000_000;
+   // parameter BAUD_RATE = 9600; 
+   //localparam BAUD_DIV = (CLK_FREQ / BAUD_RATE);
 
-    localparam DIVISOR = 10;   // TEMP FOR SIMULATION
-    reg [15:0] counter;
-
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            counter <= 0;
-            tick <= 0;
-        end else begin
-            if (counter == DIVISOR - 1) begin
-                counter <= 0;
-                tick <= 1;  // pulse
-            end else begin
-                counter <= counter + 1;
-                tick <= 0;
-            end
+    localparam BAUD_DIV = 10;
+    reg [13:0] count;
+    always @(posedge clk)
+        begin
+            if (reset | (count == BAUD_DIV))
+                count <= 0;
+            else
+                count <= count + 1; 
         end
-    end
-
+        
+        assign baud_tick = (count == BAUD_DIV);
+        
 endmodule
